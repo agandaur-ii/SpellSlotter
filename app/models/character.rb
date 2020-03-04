19a586df984de947d0b803163d86e2baefd24ee9
Agandaur <cjwagstaffe@gmail.com>
2020-03-03 16:50:04 -0800

    Added attributes to characters and spells

--- app/models/character.rb
+++ app/models/character.rb
@@ -3,4 +3,87 @@ class Character < ApplicationRecord
     belongs_to :party
     has_many :characterspells
     has_many :spells, through: :characterspells
+
+    def races 
+      [
+        "Dragonborn",
+		"Dwarf",
+		"Elf",
+		"Gnome",
+		"Half-Elf",
+		"Half-Orc",
+		"Halfling",
+		"Human",
+		"Tiefling"
+      ]
+    end
+    
+    def classes
+      [
+		"Barbarian",
+		"Bard",
+		"Cleric",
+		"Druid",
+		"Fighter",
+		"Monk",
+		"Paladin",
+		"Ranger",
+		"Rogue",
+		"Sorcerer",
+        "Warlock",
+        "Witch",
+		"Wizard"
+      ]
+    end
+
+    def backgrounds 
+      [
+        "Acolyte",
+        "Anthropologist",
+        "Archaeologist",
+        "Charlatan",
+        "City Watch / Investigator",
+        "Clan Crafter",
+        "Cloistered Scholar",
+        "Courtier",
+        "Criminal / Spy",
+        "Entertainer",
+        "Faction Agent",
+        "Far Traveler",
+        "Fisher",
+        "Folk Hero",
+        "Gladiator",
+        "Guild Artisan / Guild Merchant",
+        "Hermit",
+        "Inheritor",
+        "Inheritance",
+        "Knight",
+        "Marine",
+        "Mercenary Veteran",
+        "Noble",
+        "Outlander",
+        "Pirate",
+        "Sage",
+        "Sailor",
+        "Shipwright",
+        "Smuggler",
+        "Soldier",
+        "Urban Bounty Hunter",
+        "Urchin"
+      ]
+    end
+
+    def alignments
+      [
+        "Lawful Good",
+        "Neutral Good",
+        "Chaotic Good",
+        "Lawful Neutral",
+        "Neutral",
+        "Chaotic Neutral",
+        "Lawful Evil",
+        "Neutral Evil",
+        "Chaotic Evil"
+      ]
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   resources :spells
-  resources :characterspells
   resources :characters
   resources :parties
   resources :players
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
